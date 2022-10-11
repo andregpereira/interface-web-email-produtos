7e4e3e994f76248e3039121883b96e62cc108c13
André <61523970+andregpereira@users.noreply.github.com>
2022-10-11 12:46:14 -0300

    upload project

---
+++ src/main/java/com/bios/sprint03/interfacewebemailprodutos/InterfaceWebEmailProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.bios.sprint03.interfacewebemailprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceWebEmailProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceWebEmailProdutosApplication.class, args);
+	}
+
+}
